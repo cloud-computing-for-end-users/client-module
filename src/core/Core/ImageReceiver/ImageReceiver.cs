2a1e26ffd27516dc8fbfaad98622eebdd7da2224
gaticks <kenneth.norholm@gmail.com>
2019-09-18 14:59:27 +0200

    CC-52 cleaned and copied python image reciver

--- src/core/Core/ImageReceiver/ImageReceiver.cs
+++ src/core/Core/ImageReceiver/ImageReceiver.cs
@@ -12,13 +12,14 @@ namespace Core
     public class ImageReceiver
     {
         private static int MAX_REVICE_BUFFER_SIZE = 100000;
+
         public static void StartImageReceivingThread(ConnectionInformation connInfo, string filePath)
         {
             var t = new Thread(
-                () => { ReceiveImages(connInfo, filePath); }) {IsBackground = true};
+                () => { ReceiveImages(connInfo, filePath); })
+            { IsBackground = true };
             t.Start();
         }
-
         private static void ReceiveImages(ConnectionInformation connInfo, string filePath)
         {
             IPAddress ipAddr = IPAddress.Parse(connInfo.IP.TheIP);
@@ -27,67 +28,52 @@ namespace Core
             var reciver = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             reciver.Connect(endPoint);
             Console.WriteLine("Connection established with: " + reciver.ToString());
+            ReceiveImages(reciver, filePath);
+        }
 
-            //reciver.Listen(10)/*;*/
-            //var connection = reciver.Accept();
-            string fileName = string.Empty;
+        private static void ReceiveImages(Socket reciver, string filePath)
+        {
+            var imageSizeBuffer = new byte[sizeof(int)];
 
-            var startTime = DateTime.Now;
-            Console.WriteLine("Starting to recive images at: " + startTime);
+            reciver.Receive(imageSizeBuffer);
+            var imageDataSize = BitConverter.ToInt32(imageSizeBuffer, 0);
 
-            while (true)
+            Console.WriteLine("Image size in byte from python: " + imageSizeBuffer.ToString());
+
+            if (0 == imageDataSize)
             {
-                //if(images_to_recive_before_breakup == counter)
-                //{
-                //    Console.WriteLine("Recived " + images_to_recive_before_breakup + " images in : " + DateTime.Now.Subtract(startTime));
-                //    break;
-                //}
-                var imageSizeBuffer = new byte[sizeof(int)];
-
-                reciver.Receive(imageSizeBuffer);
-
-                var imageDataSize = BitConverter.ToInt32(imageSizeBuffer, 0);
-                Console.WriteLine("Image size in byte from python: " + imageSizeBuffer.ToString());
-                Console.WriteLine("Expecting image of: " + imageDataSize + "bytes");
-                if (0 == imageDataSize)
-                {
-                    Console.WriteLine("Recived an image size of 0 so am skipping save to file");
-                    continue;
-                }
-
-                //fileName = ++counter + ".jpg";
-                fileName = "img.jpg";
-                using (var fs = new FileStream(filePath + fileName, FileMode.Create, FileAccess.Write))
-                {
-                    while ((imageDataSize - MAX_REVICE_BUFFER_SIZE) > 0)
-                    {
-                        Console.WriteLine("Remaining data to recive: " + imageDataSize);
-
-                        byte[] fileBuffer = new byte[MAX_REVICE_BUFFER_SIZE];
-
-                        imageDataSize -= MAX_REVICE_BUFFER_SIZE;
-                        //read maxReciveSize
-                        reciver.Receive(fileBuffer);
-                        fs.Write(fileBuffer);
-                    }
-
-                    if (imageDataSize > 0)
-                    {
-
-                        var imageBuffer = new byte[imageDataSize];
-                        reciver.Receive(imageBuffer);
-                        imageDataSize = 0;
-                        fs.Write(imageBuffer, 0, imageBuffer.Length);
-                    }
-
-                    Console.WriteLine("Remaining data to recive: " + imageDataSize);
-
-                    fs.Flush();
-                    fs.Close();
-                }
-
-                Console.WriteLine("Saved a file that was recived from python");
+                Console.WriteLine("Recived an image size of 0 so am skipping save to file");
+                //recursion
+                ReceiveImages(reciver, filePath);
             }
+
+            //fileName = ++counter + ".jpg";
+            string fileName = "img.jpg";
+            using (var fs = new FileStream(filePath + fileName, FileMode.Create, FileAccess.Write))
+            {
+                int totalRecivedData = 0;
+                byte[] fileBuffer = null;
+                while (imageDataSize > totalRecivedData)
+                {
+                    Console.WriteLine("Remaining data to recive: " + imageDataSize);
+                    fileBuffer = new byte[MAX_REVICE_BUFFER_SIZE];
+
+                    //read maxReciveSize
+                    int bytesToRead = imageDataSize - totalRecivedData > MAX_REVICE_BUFFER_SIZE ? MAX_REVICE_BUFFER_SIZE : imageDataSize - totalRecivedData;
+                    var recivedBytes = reciver.Receive(fileBuffer, 0, bytesToRead, SocketFlags.None);
+                    totalRecivedData += recivedBytes;
+                    Console.WriteLine("Amount of recived bytes so far: " + totalRecivedData);
+                    fs.Write(fileBuffer, 0, recivedBytes);
+                }
+
+                fs.Flush();
+                fs.Close();
+            }
+
+            Console.WriteLine("Saved a file that was recived from python");
+
+            //recursion
+            ReceiveImages(reciver, filePath);
         }
     }
 }
