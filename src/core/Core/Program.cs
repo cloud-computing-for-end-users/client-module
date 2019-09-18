ec0e524abfa22dbe7be427776140af42f5104902
Krystof Spiller <253812@via.dk>
2019-09-18 13:00:58 +0200

    CC-10 Removing the log file every time the app runs

--- src/core/Core/Program.cs
+++ src/core/Core/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
 using NLog;
 
 namespace Core
@@ -17,9 +19,16 @@ namespace Core
         static void SetupNLog()
         {
             var config = new NLog.Config.LoggingConfiguration();
+            var logFile = "log.txt";
+            var rootFolder = System.AppDomain.CurrentDomain.BaseDirectory;
 
+            if (File.Exists(Path.Combine(rootFolder, logFile)))
+            {  
+                File.Delete(Path.Combine(rootFolder, logFile));
+            }
+        
             // Targets where to log to: File and Console
-            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "file.txt" };
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = logFile };
 
             // Rules for mapping loggers to targets            
             config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
