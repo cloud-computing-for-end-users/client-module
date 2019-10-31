038fec1a30753a6d7549eabf13f84a9ef8459a0d
Krystof Spiller <253812@via.dk>
2019-10-31 13:45:15 +0100

    CC-107 Communication module polls until rename, download and upload of file are acknowledged

--- src/communication-module/communication-module/ExternalComms/FileHandler.cs
+++ src/communication-module/communication-module/ExternalComms/FileHandler.cs
@@ -16,6 +16,9 @@ namespace Core.ExternalComms
         private FileServermoduleProxy FileServermoduleProxy { get; }
 
         private List<FileName> _listOfFileNames;
+        private string _stateOfUpload;
+        private string _stateOfDownload;
+        private string _stateOfRename;
 
         internal FileHandler(ProxyHelper ph, ClientModuleCommunication cmm)
         {
@@ -33,37 +36,42 @@ namespace Core.ExternalComms
 
         internal string UploadFile(PrimaryKeyAndFileWrapper parameters)
         {
+            _stateOfUpload = null;
             Logger.Info("UploadFile initiated");
             var fileData = System.IO.File.ReadAllBytes(parameters.FileName); // file name is an absolute path of the uploaded file
             var actualFileName = Path.GetFileName(parameters.FileName);
             FileServermoduleProxy.UploadFile(new File{FileData = fileData, FileName = new FileName{FileNameProp = actualFileName}}, new PrimaryKey { TheKey = parameters.PrimaryKey }, false, UploadFileCallback);
-            return "Done (UploadFile)";
+            return GeneralHandler.PollVariableFor10Seconds(ref _stateOfUpload);
         }
 
         internal string DownloadFile(PrimaryKeyAndFileWrapper parameters)
         {
+            _stateOfDownload = null;
             Logger.Info("DownloadFile initiated");
             FileServermoduleProxy.DownloadFile(new FileName{FileNameProp = parameters.FileName}, new PrimaryKey{TheKey = parameters.PrimaryKey}, DownloadFileCallback);
-            return "Done (DownloadFile)";
+            return GeneralHandler.PollVariableFor10Seconds(ref _stateOfDownload);
         }
 
         internal string RenameFile(RenameFileWrapper parameters)
         {
+            _stateOfRename = null;
             Logger.Info("RenameFile initiated");
             FileServermoduleProxy.RenameFile(new FileName { FileNameProp = parameters.OldFileName }, new FileName { FileNameProp = parameters.NewFileName }, new PrimaryKey { TheKey = parameters.PrimaryKey }, RenameFileCallback);
-            return "Done (RenameFile)";
+            return GeneralHandler.PollVariableFor10Seconds(ref _stateOfRename);
         }
 
         // Callbacks
         private void GetListOfFilesCallback(List<FileName> fileNames)
         {
             _listOfFileNames = fileNames;
-            Logger.Info(this._listOfFileNames.GetType().Name + " " + nameof(this._listOfFileNames) + " set (GetListOfFilesCallback)"); 
+            Logger.Info(this._listOfFileNames?.GetType().Name + " " + nameof(this._listOfFileNames) + " set (GetListOfFilesCallback)");
         }
 
         private void UploadFileCallback()
         {
             Logger.Info("Upload of file executed (UploadFileCallback)");
+
+            _stateOfUpload = "Done (UploadFile)";
         }
 
         private void DownloadFileCallback(File file)
@@ -72,11 +80,15 @@ namespace Core.ExternalComms
             Logger.Debug("Overwriting " + path);
             System.IO.File.WriteAllBytes(path, file.FileData);
             Logger.Info("Download of file executed(DownloadFileCallback)");
+
+            _stateOfDownload = "Done (DownloadFile)";
         }
 
         private void RenameFileCallback()
         {
             Logger.Info("Rename of file executed (RenameFileCallback)");
+
+            _stateOfRename = "Done (RenameFile)";
         }
     }
 }
